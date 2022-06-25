dafbd47b11357aa3da8113c831b837c910502814
Ronaldo Montan <ronaldogithub@gmail.com>
2022-06-25 20:08:05 -0300

    Primeira subida para o Git

---
+++ tupula1.py
@@ -0,0 +1,15 @@
+
+
+tupula = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+continua = ''
+
+while continua in 'sS':    
+    while True:
+        escolha = int(input('Digete um valor entre 0 - 20:  '))
+        if escolha <= 0 or escolha <= 20:
+            break
+        print('tente novamnet...', end='')
+
+    print(tupula[escolha])
+    continua = str(input('Gostaria de continuar? '))[0]
+
