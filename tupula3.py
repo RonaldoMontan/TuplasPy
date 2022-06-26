ecd5bc6e725e4eee0f55af0fa6ae505a50adf6db
Ronaldo Montan <ronaldogithub@gmail.com>
2022-06-26 13:57:39 -0300

    comentado outras maneiras de buscar maior x menor

--- tupula3.py
+++ tupula3.py
@@ -8,3 +8,7 @@ print(sortido)
 sleep(1)
 print(f'o menor valor foi {sorted(sortido)[0]}')
 print(f'o maior valor foi {sorted(sortido)[4]}')
+
+#ou pode fazer assim
+#print(f'o maior valor foi {max(sortido)}')
+#print(f'O menor valor foi {min(sortido)}')
