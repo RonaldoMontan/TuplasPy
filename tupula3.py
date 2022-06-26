7ac6195021b9d3a96f4cf78f1a30539fb432eb1f
Ronaldo Montan <ronaldogithub@gmail.com>
2022-06-26 13:53:07 -0300

    Preenchendo um tupla com 5 numeros gerados aleatóriamente

---
+++ tupula3.py
@@ -0,0 +1,10 @@
+from random import randint
+from time import sleep
+
+print('Vou sortear 5 numeros...')
+sleep(2)
+sortido = (randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10))
+print(sortido)
+sleep(1)
+print(f'o menor valor foi {sorted(sortido)[0]}')
+print(f'o maior valor foi {sorted(sortido)[4]}')
